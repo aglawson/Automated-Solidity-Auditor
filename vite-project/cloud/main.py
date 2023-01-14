92841b4684c5c771e6f00373a1add3f63cfe3e06
Adrian Lawson <adrianglawson@gmail.com>
2023-01-13 16:11:08 -0800

    upload cloud code

---
+++ vite-project/cloud/main.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+def audit(request):
+    warning = ""
+    # Smart contract code
+    contract_code = request.args.get("contract_code")
+
+    temp_string = ""
+    for line in contract_code.strip().split("\n"):
+        temp_string += line
+        if "pragma solidity" not in contract_code:
+            warning += "Solc version not specified"
+            break;
+        if "pragma solidity" in line:
+            solc = temp_string.split("pragma solidity ")[1].split(";")[0]
+            version = int(solc.split('.')[1].split('.')[0])
+            if version < 8 and "SafeMath" not in contract_code:
+                warning += "Contracts using solc versions under 0.8.0 are vulnerable to underflow/overflow attacks if not using SafeMath library"
+    
+    if warning == "":
+        warning = "No issues detected"
+        
+    return warning
