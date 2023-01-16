72054052d120e4cde4e4cfc5f368c2d70425ba64
Adrian Lawson <adrianglawson@gmail.com>
2023-01-16 08:54:04 -0800

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -20,15 +20,7 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Automated Smart Contract Auditor</h1>
       <div className="card">
         <form onSubmit={e => callAudit(e)}>
           <input id="code" type="text" placeholder="Enter Solidity code here . . ." style={{height: "500px", width: "500px"}}></input>
